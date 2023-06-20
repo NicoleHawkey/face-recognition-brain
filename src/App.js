c7fe172ea3314bd70ed39b1c947e5cad9cf9e9c6
Nicole Hawkey <n.laggner@gmx.at>
2023-06-20 11:18:20 +0700

    multiple face detection

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ window.process = {}
 const initialState = {
     input: '',
     imageUrl: '',
-    box: {},
+    boxes: [],
     route: 'signin',
     isSignedIn: false,
     user: {
@@ -42,21 +42,23 @@ class App extends Component {
     }})
   }
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFaceLocations = (data) => {
     const image =  document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row *  height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),
-    } 
+    return data.outputs[0].data.regions.map(face => {
+      const clarifaiFace = face.region_info.bounding_box;
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row *  height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height),
+     }
+    }); 
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBoxes = (boxes) => {
+    this.setState({boxes: boxes});
   }
 
   onInputChange = (event) => {
@@ -88,7 +90,7 @@ class App extends Component {
               })
               .catch(console.log)
             }
-            this.displayFaceBox(this.calculateFaceLocation(response))
+            this.displayFaceBoxes(this.calculateFaceLocations(response))
           })
           .catch(err => console.log(err));
       }
@@ -103,7 +105,7 @@ class App extends Component {
     }
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <ParticlesBg className="particles" type="cobweb" bg={true} />
@@ -118,7 +120,7 @@ class App extends Component {
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
             </div>
            : (
               route === 'signin' 
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
 
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
 				<img id='inputimage' src={imageUrl} alt=""  width='500px' height='auto' />	
-				<div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left:  box.leftCol}} />
+				{boxes.map(box => 
+					<div 
+						key={`box${box.topRow}${box.rightCol}`}
+						className='bounding-box' 
+						style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left:  box.leftCol}} />
+				)}
 			</div>
 		</div>
 	);
